33862f9d83d98112a1b8a6ca315091bd524838e0
jeckschmidt <45171254+jeckschmidt@users.noreply.github.com>
2024-04-29 01:11:17 -0400

    Add files via upload

---
+++ groceryItems.java
@@ -0,0 +1,22 @@
+package application;
+
+
+
+public class groceryItems {
+
+	// object attributes
+	private int ID;
+	private float cost;
+	
+	
+	// get methods
+	public int getID() {
+		return this.ID;
+	}
+	
+	public float getCost() {
+		return this.cost;
+	}
+	
+		
+}
